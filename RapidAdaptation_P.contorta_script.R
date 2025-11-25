e72325cedc4c2c05b1032c6c652d490ae3be699a
Luisa Dickenmann <LuiDnn@ceh.ac.uk>
2025-11-25 12:30:45 +0000

    cleaned up dataset and converted into Julian days

--- RapidAdaptation_P.contorta_script.R
+++ RapidAdaptation_P.contorta_script.R
@@ -15,11 +15,12 @@
 
 
 #' Biological questions:
-#' Do provenance and budburst/budset/height/root:shoot ratio correlate?
+#' Do provenance/family and budburst/budset/height/root:shoot ratio correlate?
 #' first need to know distribution of all variables and traits
 
 # load libraries & import data --------------------------------------------
-
+library(dplyr)
+library(dplyr)
 library(readxl)
 data <- read_excel("P:/07793_newLEAF/Workfiles/WP4/RapidAdaptationTrial_MasterSheet.xlsx", sheet = "Pinus contorta")
 
@@ -28,24 +29,123 @@ str(data)
 ncol(data)
 # 28 columns
 colnames(data)
-#' "Block"                "Position"             "Column"              
-#' "Row"                  "Type"                 "Provenance"          
-#' "Provenance region"    "Collection site (UK)" "Family"              
-#' "Status"               "Date of budset"       "Date of budburst"    
-#' "DBB (mm)...13"        "Height (cm)...14"     "Adjusted Height (mm)"
-#' "Needle length 1 (mm)" "Needle length 1 (cm)" "Needle length 2 (mm)"
-#' "Needle length 2 (cm)" "Date"                 "Ruler"               
-#' "Offset"               "Status 22/09/2025"    "Size"                
-#' "DBB (mm)...25"        "Date measured DBB"    "Height (cm)...27"    
-#' "Date measured"       
+unique(data$`Date of budset`)
 
-
+#' "Block": 1-10                
+#' "Position": 1-78             
+#' "Column": 1-8               
+#' "Row": 10                   
+#' "Type": Origin/Plantation/Regeneration                 
+#' "Provenance": North Coast/Skeena River/Alaska          
+#' "Provenance region": unknown/20104/32151/46134/47475/48744/52140
+#' "Collection site (UK)": NA/Benmore/Rowens
+#' "Family": e.g., LPNC-UKA5, LPSR - Ca6, etc. (55 families)               
+#' "Status" dead/alive 
+#'      -> CHANGE TO STATUS_1
+#' "Date of budset": NA/September-December 2023, January-May 2024 
+#'      -> CHANGE COLUMN NAME TO date_budset
+#'      -> CHANGE TO JULIAN DAYS? does this work with dates across the new year?    
+#' "Date of budburst": NA/February-March 2024 
+#'      -> change column name to date_budburst
+#'      -> change to Julian days    
+#' "DBB (mm)...13" 
+#'    -> change to DBB_1
+#'    -> change to numerical       
+#' "Height (cm)...14"   
+#'    -> change to Height_1
+#'    -> change to numerical
+#'    -> remove  
+#' "Adjusted Height (mm)"
+#'   -> use this
+#'   -> change to height
+#' "Needle length 1 (mm)" 
+#' "Needle length 1 (cm)" 
+#' "Needle length 2 (mm)"
+#' "Needle length 2 (cm)" 
+#'    -> check which one to use
+#' "Date" 
+#'    -> change to "date_1"  
+#'    -> or is this date for dbb, height and needle measurements?              
+#' "Ruler": Oxford/Yellow/Red Ruler              
+#' "Offset": 0.4/0.6              
+#' "Status 22/09/2025"
+#'    -> change to status_2    
+#' "Size"
+#'     -> remove               
+#' "DBB (mm)...25"
+#'    -> change to DBB_2
+#'    -> change to numeric        
+#' "Date measured DBB"
+#'    -> change to date_DBB_2   
+#'    -> change to Julian days        
+#' "Height (cm)...27"
+#'    -> change to height_2
+#'    -> change to numeric    
+#' "Date measured"
+#'    -> change to date_height_2
+#'    -> change to Julian days       
 
 # TO DO:
 #' change date to days since... (especially for budburst and budset measurements)
 #' check range & distribution of numerical values
-#' remove NAs
+#' remove NAs?
 #' distinguish better between first and second measurements (e.g., DBB_1, DBB_2)
 
-hist(data$`DBB (mm)...13`)
-range(data$`DBB (mm)...13`)
+LP <- data %>% 
+  rename(
+    "status_1" = "Status",
+    "date_budset" = "Date of budset",
+    "date_budburst" = "Date of budburst",
+    "DBB_1" = "DBB (mm)...13",
+    "height_1" = "Adjusted Height (mm)",
+    "needle_1" = "Needle length 1 (mm)",
+    "needle_2" = "Needle length 2 (mm)",
+    "date_1" = "Date",
+    "status_2" = "Status 22/09/2025",
+    "DBB_2" = "DBB (mm)...25",
+    "date_DBB_2" = "Date measured DBB",
+    "height_2" = "Height (cm)...27",
+    "date_height_2" = "Date measured"
+  )  %>% 
+  select(-c(`Height (cm)...14`, `Needle length 1 (cm)`, `Needle length 2 (cm)`, Ruler, Offset, Size)) %>% 
+  mutate(across(c(DBB_1, height_1, needle_1, needle_2, height_2, DBB_2),as.numeric))
+    # NAs introduced by coercion
+
+# CONVERT INTO JULIAN DAYS
+  # 
+  # probably won't work because later burbursts will have smaller Julian day values?
+
+# Budset
+range(LP$date_budset, na.rm = T)
+#' year/month/day
+#' 2023/09/11 - 2024/05/17 
+#' 
+LP$date_budset <- as.Date(LP$date_budset)
+LP$Julian_budset <- julian(LP$date_budset, origin = as.Date("2023-09-10"))
+  # set origin date as one day before first budset so that Julian day value is 1 instead of 0
+
+# Budburst
+
+range(LP$date_budburst, na.rm = T)
+#' 2024/02/02 - 2024/05/22
+
+LP$date_budburst <- as.Date(LP$date_budburst)
+LP$Julian_budburst <- julian(LP$date_budburst, origin = as.Date("2024-02-01"))
+
+# check with Annika if this makes sense
+#' do both budset and budburst need to have the same origin day?
+#' budset starts earlier so if tree A budset and tree B budburst are on same calendar day, Julian day would be different
+
+
+# Data exploration --------------------------------------------------------
+
+
+sum(is.na(data$`DBB (mm)...13`))
+sum(data$`DBB (mm)...13` == "na", na.rm =T)
+  # 27
+sum(is.na(LP$DBB_1))
+  # 27
+
+range(LP$DBB_1, na.rm = T)
+  # 0.12 - 0.94
+
